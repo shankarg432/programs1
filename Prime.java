3c2b289b5cd4603ce09754569f5e5d7b6ff43c2a
shankarg432 <sg1421167@gmail.com>
2017-07-06 17:37:54 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) { 
+			  int i,m=0,flag=0;    
+			  Scanner s=new Scanner(System.in);
+			  int n=s.nextInt();
+			  m=n/2;    
+			  for(i=2;i<=m;i++){    
+			   if(n%i==0){    
+			   System.out.println("Number is not prime");    
+			   flag=1;    
+			   break;    
+			   }    
+			  }    
+			  if(flag==0)  
+			  {
+			  System.out.println("Number is prime");
+			  }
+
+	}
+
+}
